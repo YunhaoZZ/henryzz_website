7629e94e68e347e8702cdeb393fa144a3058a219
YunhaoZZ <zhangenochyun@outlook.com>
2023-04-05 00:26:35 -0500

    ec2 removed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { ContextProvider } from './components/Desktop/context/Context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <ContextProvider>
         <App />
       </ContextProvider>
