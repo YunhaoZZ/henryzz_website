23ef6eb2b7ab81eb43436280c982d6dcdeb9f641
YunhaoZZ <zhangenochyun@outlook.com>
2023-05-24 03:12:17 -0500

    added projects page

--- src/components/Desktop/pages/Projects/index.js
+++ src/components/Desktop/pages/Projects/index.js
@@ -18,6 +18,34 @@ const Projects = () => {
             clearTimeout(timer);
         }
     });
+
+    const renderProject = (projects) => {
+        return (
+            <div className="images-container">
+                {
+                    projects.map((port, idx) => {
+                        return (
+                            <div className="image-box" key={idx}>
+                                <img 
+                                src={port.cover}
+                                className="project-image"
+                                alt="project" />
+                                <div className="content">
+                                    <p className="title">{port.title}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button
+                                        className="btn"
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        );
+    }
+
     return (
         <>
             <div className="container projects-page">
@@ -28,7 +56,7 @@ const Projects = () => {
                         idx={15}
                     />
                 </h1>
-                {/* <div>{renderPortfolio(portfolio)}</div> */}
+                <div>{renderProject(projectsData.projects)}</div>
             </div>
             <Loader type="square-spin" />
         </>
