c9520684672d9ab8f0fcd32da59a3e3f82dc8686
YunhaoZZ <zhangenochyun@outlook.com>
2023-05-25 01:21:29 -0500

    Updates

---
+++ api/posts.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+import http from "http";
+const { Client } = require('@notionhq/client');
+
+const notion = new Client({
+    auth: process.env.NOTION_API_TOKEN,
+});
+
+export default async () => {
+    const databaseId = '668d797c-76fa-4934-9b05-ad288df2d136';
+    const response = await notion.databases.retrieve({ database_id: databaseId });
+    console.log(response);
+};
